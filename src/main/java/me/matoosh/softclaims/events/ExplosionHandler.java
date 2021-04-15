4309dbfed6f14f7d2e519002b93477143c28f30d
Mateusz Rębacz <m.rebacz@student.tudelft.nl>
2021-04-16 00:31:43 +0200

    Add breaking obsidian with explosions.

--- src/main/java/me/matoosh/softclaims/events/ExplosionHandler.java
+++ src/main/java/me/matoosh/softclaims/events/ExplosionHandler.java
@@ -2,6 +2,7 @@ package me.matoosh.softclaims.events;
 
 import me.matoosh.softclaims.SoftClaimsPlugin;
 import me.matoosh.softclaims.exception.ChunkBusyException;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
@@ -10,15 +11,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 public class ExplosionHandler implements Listener {
 
     private final SoftClaimsPlugin plugin;
+
+    private static final int EXPLOSION_RADIUS = 3;
 
     public ExplosionHandler(SoftClaimsPlugin plugin) {
         this.plugin = plugin;
@@ -44,7 +45,7 @@ public class ExplosionHandler implements Listener {
             return;
         }
 
-        Set<Block> durableBlocks = new HashSet<>(); // durable blocks that should be processed further
+        List<Block> durableBlocks = new ArrayList<>(); // durable blocks that should be processed further
         Set<Chunk> skipChunks = new HashSet<>(); // chunks not in faction land that should be skipped
         Set<Chunk> actChunks = new HashSet<>(); // chunks in faction land that should be processed
         Iterator<Block> blockIterator = blockList.iterator();
@@ -73,39 +74,72 @@ public class ExplosionHandler implements Listener {
                 blockIterator.remove();
             }
         }
-        if (durableBlocks.size() == 0) return;
 
-        // get power
-        int power = powerFunction.apply(null);
+        // process durable block further asynchronously
+        processDurableBlocksAsync(durableBlocks, location, powerFunction);
+    }
 
-        // apply damage to blocks
-        for (Block b : durableBlocks) {
-            int durability;
-            try {
-                durability = plugin.getBlockDurabilityService().getDurabilityAbsolute(b);
-            } catch (ChunkBusyException e) {
-                continue;
-            }
+    private CompletableFuture<Void> processDurableBlocksAsync(
+            List<Block> durableBlocks, Location location,
+            Function<Void, Integer> powerFunction) {
+        return CompletableFuture.runAsync(() -> {
+            // get blocks around
+            for (int x = -EXPLOSION_RADIUS; x <= EXPLOSION_RADIUS; x++) {
+                for (int y = -EXPLOSION_RADIUS; y <= EXPLOSION_RADIUS; y++) {
+                    for (int z = -EXPLOSION_RADIUS; z <= EXPLOSION_RADIUS; z++) {
+                        if (x == 0 && y == 0 && z == 0) continue;
 
-            // calculate damage to block based on distance
-            // to the center of the explosion
-            double dist = location.distanceSquared(b.getLocation());
-            // 2 and 0.7 ensure that explosion 1 block away from the block
-            // damaged the block with the full power. Explosions inside of the
-            // block will result in double the power being exerted.
-            durability -= 2 * power + Math.exp(-0.7d * dist);
-            if(durability > 0) {
-                // update durability
-                try {
-                    plugin.getBlockDurabilityService().setDurabilityAbsolute(b, durability);
-                } catch (ChunkBusyException e) {
-                    e.printStackTrace();
+                        Block block = location.clone().add(x, y, z).getBlock();
+                        if (block.getType().getBlastResistance() >= 1200) {
+                            // block that doesnt appear in the normal explosion list
+                            if (plugin.getBlockDurabilityService().hasDurability(block)) {
+                                durableBlocks.add(block);
+                            }
+                        }
+                    }
                 }
-            } else {
-                // block exploded
-                plugin.getBlockDurabilityService().clearDurability(b);
-                blockList.add(b);
             }
-        }
+
+            // check if we found any blocks
+            if (durableBlocks.size() == 0) return;
+
+            // get power
+            int power = powerFunction.apply(null);
+
+            // apply damage to blocks
+            List<Block> destroyedBlocks = new ArrayList<>();
+            for (Block b : durableBlocks) {
+                int durability;
+                try {
+                    durability = plugin.getBlockDurabilityService().getDurabilityAbsolute(b);
+                } catch (ChunkBusyException e) {
+                    continue;
+                }
+
+                // calculate damage to block based on distance
+                // to the center of the explosion
+                double dist = location.distance(b.getLocation().toCenterLocation()) - 1;
+                // 2 and 0.7 ensure that explosion 1 block away from the block
+                // damaged the block with the full power. Explosions inside of the
+                // block will result in double the power being exerted.
+                durability -= power * Math.exp(-dist);
+                if(durability > 0) {
+                    // update durability
+                    try {
+                        plugin.getBlockDurabilityService().setDurabilityAbsolute(b, durability);
+                    } catch (ChunkBusyException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    // block exploded
+                    plugin.getBlockDurabilityService().clearDurability(b);
+                    destroyedBlocks.add(b);
+                }
+            }
+
+            // break blocks
+            Bukkit.getScheduler().runTask(plugin, () -> destroyedBlocks.forEach(Block::breakNaturally));
+        });
+
     }
 }
